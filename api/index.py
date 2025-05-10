9c8fbec1b47b3e4af3b4004037055e6d848c3212
VrBnDev <rafaelavborgess@gmail.com>
2025-05-10 07:13:01 -0300

    :arrow_up: Adicionando biblioteca SocketIO para tentar receber dados da placa

--- api/index.py
+++ api/index.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, template_folder="templates")
+
+# Configura o SocketIO para permitir conexões de qualquer origem
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+# Rota para comando "pressed"
+@app.route('/pressed', methods=['GET', 'POST'])
+def pressed():
+    print("Comando: pressed")  # Imprime no console (útil para debug)
+    socketio.emit('command', {'action': 'pressed'})  # Envia comando via WebSocket
+    return 'Pressed command sent', 200  # Retorna uma resposta HTTP padrão
+
+# Rota para comando "unpressed"
+@app.route('/unpressed', methods=['GET', 'POST'])
+def unpressed():
+    print("Comando: Unpressed")  # Imprime no console (útil para debug)
+    socketio.emit('command', {'action': 'unpressed'})  # Envia comando via WebSocket
+    return 'Unpressed command sent', 200  # Retorna uma resposta HTTP padrão
 
 # Vercel vai usar a variável "app" como ponto de entrada
