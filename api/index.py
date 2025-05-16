8142f3a0c11ca45d76c200358ce3bedfc059e23b
Vitória Nascimento <110772119+VrBnDev@users.noreply.github.com>
2025-05-16 13:56:48 -0300

    Atualizações nas rotas do servidor

--- api/index.py
+++ api/index.py
@@ -7,17 +7,17 @@ estado_botao = "unpressed"
 def index():
     return render_template('index.html')
 
-@app.route('/pressed')
+@app.route('/pressed', methods=['GET','POST'])
 def pressed():
     global estado_botao
     estado_botao = "pressed"
-    return jsonify({"message": "Estado atualizado para pressionado"})
+    return jsonify({"action": estado_botao, "message": "Estado atualizado para pressionado"})
 
-@app.route('/unpressed')
+@app.route('/unpressed', methods=['GET','POST'])
 def unpressed():
     global estado_botao
     estado_botao = "unpressed"
-    return jsonify({"message": "Estado atualizado para solto"})
+    return jsonify({"action": estado_botao, "message": "Estado atualizado para solto"})
 
 @app.route('/status')
 def status():
