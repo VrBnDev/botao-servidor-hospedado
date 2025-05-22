4441b55e905dd6131108406bf44f37a38001a525
VrBnDev <rafaelavborgess@gmail.com>
2025-05-22 12:15:11 -0300

    :fire: Removendo rotas não utilizadas

--- api/index.py
+++ api/index.py
@@ -7,18 +7,6 @@ estado_botao = "unpressed"
 def index():
     return render_template('index.html')
 
-@app.route('/pressed', methods=['GET','POST'])
-def pressed():
-    global estado_botao
-    estado_botao = "pressed"
-    return jsonify({"action": estado_botao, "message": "Estado atualizado para pressionado"})
-
-@app.route('/unpressed', methods=['GET','POST'])
-def unpressed():
-    global estado_botao
-    estado_botao = "unpressed"
-    return jsonify({"action": estado_botao, "message": "Estado atualizado para solto"})
-
 @app.route('/status')
 def status():
     return jsonify({"action": estado_botao, "message": "Vinda do status"})
